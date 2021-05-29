eff308c61c3c8384bf520670bd4fe7629c200b53
Oleksii Tkachenko <zlukapraporr@gmail.com>
2021-05-29 18:18:50 +0300

    Revert "v1.5"
    This reverts commit a4ec9bb7cf77031238ce507f37d2d753cb09e26a.

--- src/code.js/color-switch.js
+++ src/code.js/color-switch.js
@@ -1,48 +1,52 @@
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
-];
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
 
-const refs = {
-  start: document.querySelector('button[data-action="start"]'),
-  stop: document.querySelector('button[data-action="stop"]'),
-  bodyColor: document.querySelector("body"),
+  const refs = {
+      start: document.querySelector('button[data-action="start"]'),
+      stop: document.querySelector('button[data-action="stop"]'),
+      bodyColor: document.querySelector('body'),
+  }
+
+  const TIME_DELAY = 1000;
+  let intervalId = null;
+
+  refs.start.addEventListener('click', startColorSwitch);
+  refs.stop.addEventListener('click', stopColorSwitch);
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+function randomColor () {
+    const randomCol = colors[randomIntegerFromInterval(0, colors.length-1)];
+   console.log(randomCol);
+   
+    refs.bodyColor.style.backgroundColor = randomCol;
+    
 };
 
-const TIME_DELAY = 1000;
-let intervalId = null;
+  function startColorSwitch (e) {
+    
+    intervalId = setInterval(() => {
+        randomColor();
 
-refs.start.addEventListener("click", startColorSwitch);
-refs.stop.addEventListener("click", stopColorSwitch);
+    }, TIME_DELAY);
+    e.target.disabled = 'true';
+    refs.start.removeEventListener('click', startColorSwitch);
+    refs.stop.addEventListener('click', stopColorSwitch);
+  };
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+  function stopColorSwitch (e) {
+    clearInterval(intervalId);
 
-function randomColor() {
-  const randomCol = colors[randomIntegerFromInterval(0, colors.length - 1)];
-  console.log(randomCol);
+    refs.start.removeAttribute('disabled');
+    refs.start.addEventListener('click', startColorSwitch);
+    refs.stop.removeEventListener('click', stopColorSwitch);
 
-  refs.bodyColor.style.backgroundColor = randomCol;
-}
-
-function startColorSwitch(e) {
-  intervalId = setInterval(() => {
-    randomColor();
-  }, TIME_DELAY);
-  e.target.disabled = "true";
-  refs.start.removeEventListener("click", startColorSwitch);
-  refs.stop.addEventListener("click", stopColorSwitch);
-}
-
-function stopColorSwitch(e) {
-  clearInterval(intervalId);
-
-  refs.start.removeAttribute("disabled");
-  refs.start.addEventListener("click", startColorSwitch);
-  refs.stop.removeEventListener("click", stopColorSwitch);
-}
+  };
